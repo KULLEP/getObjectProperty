713c7b2dd4c087e672bc3dfaf4ebde2a2f72a6c0
KULLEP <malyukow.maksim@mail.ru>
2021-11-17 17:51:53 +0200

    index

---
+++ index.js
@@ -0,0 +1,6 @@
+getObjectProperty = (obj, path, defaultValue) => {
+    let arrPath = typeof path === 'string' ? path.split('.') : []; // ['pupa', 'lupa']
+    let thisObj = { ...obj };
+    arrPath.forEach((e) => thisObj = thisObj && thisObj[e]);
+    return thisObj || defaultValue || undefined;
+}
