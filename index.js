488f0ef79a528391181175d1ae9ee8c889826f74
DrAssExplosion <50326111+KULLEP@users.noreply.github.com>
2021-12-09 22:10:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ getObjectProperty = (obj, path, defaultValue) => {
     let arrPath = typeof path === 'string' ? path.split('.') : []; // ['pupa', 'lupa']
     let thisObj = { ...obj };
     arrPath.forEach((e) => thisObj = thisObj && thisObj[e]);
-    return thisObj || defaultValue || undefined;
+    return thisObj ?? defaultValue || undefined;
 }
 
 
