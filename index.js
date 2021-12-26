7c30032ab49662cfcd73e127ce46553471d451c5
DrAssExplosion <50326111+KULLEP@users.noreply.github.com>
2021-12-26 22:15:33 +0200

    upgrade

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-getObjectProperty = (obj, path, defaultValue) => {
+const getObjectProperty = (obj, path, defaultValue) => {
     let arrPath = typeof path === 'string' ? path.split('.') : []; // ['pupa', 'lupa']
     let thisObj = { ...obj };
     arrPath.forEach((e) => thisObj = thisObj && thisObj[e]);
-    return thisObj ?? defaultValue || undefined;
+    return thisObj ??= defaultValue || undefined;
 }
 
+const getObjectProperty2 = (object, path, defaultValue) => {
+    let arrPath = typeof path === 'string' ? path.split('.') : [];
+    let obj = { ...object };
+    let result;
+    for(let i = 0; obj; obj = obj && obj[arrPath[i++]]) {
+        result = obj;
+}
+    return result ??= defaultValue || undefined;;
+}
 
